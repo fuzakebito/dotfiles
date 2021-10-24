e273ea48d1d2feee007ea6dd83a2f4dc3a816f32
shanotinohito <fuzakebito@ArchNTB.localdomain>
2021-10-24 21:23:08 +0900

    added autobuild

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,11 @@ set -x FZF_DEFAULT_COMMAND fd --type f
 set -x FZF_CTRL_T_COMMAND fd --type f
 set -x FZF_ALT_C_COMMAND fd --type d
 set -x BAT_THEME base16
-abbr inst "yay -S"
-abbr unst "yay -Rns"
+alias inst "yay -S"
+alias unst "yay -Rns"
 abbr su "su -p"
 abbr sudo "sudo -E"
 abbr uqwatch "watch -n 60 -d uqzaiko"
-abbr ls "exa"
+abbr ls "lsd"
 abbr r "ranger"
 abbr f " fuck"
