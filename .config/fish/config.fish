e4912a972ab93ad36d009032acc390f2edcdfcf4
shanotinohito <fuzakebito@ArchNTB.localdomain>
2021-11-26 23:16:34 +0900

    added new colorscheme for alacritty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 fish_vi_key_bindings
 # fzf
 set -U FZF_LEGACY_KEYBINDINGS 0
-set -U FZF_COMPLETE 3
 set -U FZF_ENABLE_OPEN_PREVIEW 1
 set -U FZF_FIND_FILE_COMMAND "fd --type f . \$dir"
 
@@ -14,3 +13,4 @@ abbr uqwatch "watch -n 60 -d uqzaiko"
 abbr ls "lsd"
 abbr r "ranger"
 abbr f " fuck"
+abbr nfct "neofetch ; colortest"
