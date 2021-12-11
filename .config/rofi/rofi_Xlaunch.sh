5ce011e3784d616c560e19cd9874bb0ab490d16a
shanotinohito <fuzakebito@ArchNTB.localdomain>
2021-12-11 11:52:31 +0900

    removed unnecessary files.

--- .config/rofi/rofi_Xlaunch.sh
+++ .config/rofi/rofi_Xlaunch.sh
@@ -4,4 +4,4 @@ export GDK_BACKEND=x11
 export QT_QPA_PLATFORM=xcb
 export SDL_VIDEODRIVER=x11
 export XINIT_UNIX_BACKEND=x11
-rofi -modi "ssh,combi,run" -modi combi -show combi -combi-modi window,drun,run -disable-history -theme ~/.config/rofi/sidetab -font "Jost* Light 15"
+rofi -modi "combi,drun,run,window,ssh" -modi combi -show combi -combi-modi window,drun,run -theme Black-Simplicity -font "Jost* Light 15"
--- .config/rofi/rofi_launch.sh
+++ .config/rofi/rofi_launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -modi "combi,drun,run,window,ssh" -modi combi -show combi -combi-modi window,drun,run -theme ~/.config/rofi/Black-Simplicity -font "Jost* Light 15"
+rofi -modi "combi,drun,run,window,ssh" -modi combi -show combi -combi-modi window,drun,run -theme Black-Simplicity -font "Jost* Light 15"
