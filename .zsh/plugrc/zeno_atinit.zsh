e59faf00b50d56c5976f40e67edbdc69480c30a7
shanotinohito <fuzakebito@gmail.com>
2022-02-01 18:24:20 +0900

    [zsh] zenoの設定を整理

--- .zsh/plugrc/zeno_atinit.zsh
+++ .zsh/plugrc/zeno_atinit.zsh
@@ -1,12 +1,9 @@
 export ZENO_HOME=$HOME/.zsh/zeno
 
-# if disable deno cache command when plugin loaded
-# export ZENO_DISABLE_EXECUTE_CACHE_COMMAND=1
-
-# if enable fzf-tmux
+# enable fzf-tmux
 export ZENO_ENABLE_FZF_TMUX=1
 
-# if setting fzf-tmux options
+# setting fzf-tmux options
 export ZENO_FZF_TMUX_OPTIONS="-p 70%"
 
 # Experimental: Use UNIX Domain Socket
