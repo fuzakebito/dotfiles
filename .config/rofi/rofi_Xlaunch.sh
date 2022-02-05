28f5fd579401cbe3269139520ab33a2ff55912f8
fuzakebito <fuzakebito@gmail.com>
2022-02-05 14:40:31 +0900

    [rofi] アップデートへの対応

--- .config/rofi/rofi_Xlaunch.sh
+++ .config/rofi/rofi_Xlaunch.sh
@@ -4,4 +4,4 @@ export GDK_BACKEND=x11
 export QT_QPA_PLATFORM=xcb
 export SDL_VIDEODRIVER=x11
 export XINIT_UNIX_BACKEND=x11
-rofi -modi "combi,drun,run,window,ssh" -modi combi -show combi -combi-modi window,drun,run -theme Black-Simplicity -font "Jost* Light 15" -terminal "env WINIT_UNIX_BACKEND=x11 alacritty"
+rofi -modi "combi,drun,run,window,ssh" -show combi -combi-modi window,drun,run -theme Black-Simplicity -font "Jost* Light 15" -terminal "env WINIT_UNIX_BACKEND=x11 alacritty"
--- .config/rofi/rofi_launch.sh
+++ .config/rofi/rofi_launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -modi "combi,drun,run,window,ssh" -modi combi -show combi -combi-modi window,drun,run -theme Black-Simplicity -font "Jost* Light 15" -terminal "env WINIT_UNIX_BACKEND=x11 alacritty"
+rofi -modi "combi,drun,run,window,ssh" -show combi -combi-modi window,drun,run -theme Black-Simplicity -font "Jost* Light 15" -terminal "env WINIT_UNIX_BACKEND=x11 alacritty"
