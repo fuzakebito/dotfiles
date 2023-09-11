d09325172c428c01751b6ddc9ee93c7aa7ebf52f
Naoya Hayashi <fuzmare@pm.me>
2023-09-11 19:15:08 +0900

    zfによるディレクトリファジーファインダー

--- .config/zsh/lazy.zsh
+++ .config/zsh/lazy.zsh
@@ -5,6 +5,7 @@ export EDITOR=nvim
 export SHELL=/bin/zsh
 export BAT_THEME=base16
 export MANPAGER='nvim +Man!'
+export ZF_PROMPT=`echo "\033[36mzf ❯ \033[m"`
 # colorize
 export LESS_TERMCAP_mb=$'\e[1;32m'
 export LESS_TERMCAP_md=$'\e[1;32m'
@@ -17,6 +18,18 @@ export LESS_TERMCAP_us=$'\e[1;4;31m'
 alias exa='exa -F --group-directories-first --icons --git'
 alias toilet='toilet -w $(tput cols)'
 # functions
+zle-cd() {
+  local dir=$(z | while IFS= read -r line; do echo ${line##* }; done | zf)
+  if [ -z "$dir" ];then
+    zle reset-prompt
+    return 0
+  fi
+  BUFFER="cd $dir"
+  zle reset-prompt
+  zle accept-line
+}
+zle -N zle-cd
+bindkey '^j' zle-cd
 # hooks
 chpwd() {
   if [[ $(pwd) != $HOME ]]; then;
