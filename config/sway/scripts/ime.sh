a463473cf2967a0a99f35bd44851621b5926935c
Naoya Hayashi <fuzmare@pm.me>
2024-04-08 01:35:58 +0900

    feat(sway): foot -> alacritty underlineの色やundercurl対応のため 今度はalacrittyにはsixelがない痛し痒し

--- config/sway/scripts/ime.sh
+++ config/sway/scripts/ime.sh
@@ -2,7 +2,7 @@
 FloatingVim=$(swaymsg -t get_tree | jq -c '.. | .floating_nodes? | arrays[] | select(.app_id=="FloatingVim")')
 
 if [ -z $FloatingVim ]; then
-  footclient -a FloatingVim -W 100x20 -o colors.alpha=0.8 -o pad=1x1 -e env ime=1 PATH=~/.deno/bin:$PATH nvim
+  alacritty --class FloatingVim -o window.dimensions.columns=100 -o window.dimensions.lines=20 -o window.opacity=0.8 -e env ime=1 PATH=~/.deno/bin:$PATH nvim
 else
   if [ "$(echo $FloatingVim | jq .focused)" = 'true' ]; then
     swaymsg "move window to scratchpad"
