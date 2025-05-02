3fbec5aa9e297db407ea8012d5e58d2804e1e7a6
HAYASHI Naoya <fuzakebito@fuzake.io>
2025-05-02 06:38:43 +0000

    fix(nvim): latexのtreesitterは表示がよくないので出荷

--- config/nvim/rc/hooks/treesitter.lua
+++ config/nvim/rc/hooks/treesitter.lua
@@ -5,10 +5,10 @@ require('nvim-treesitter.configs').setup {
   ensure_installed = {}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   auto_install = true,
-  ignore_install = {}, -- List of parsers to ignore installing
+  ignore_install = {"latex"}, -- List of parsers to ignore installing
   highlight = {
     enable = true,              -- false will disable the whole extension
-    disable = {},  -- list of language that will be disabled
+    disable = {"latex"},  -- list of language that will be disabled
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
